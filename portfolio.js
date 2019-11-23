b6f96fa0ff77ec20d2ff41481c9cb3165c5bec9f
Dustin Martinez <martinezdustin736@gmail.com>
2019-11-22 23:05:32 -0600

    updated

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+
+    $("#hangman").on("click", function(){
+     window.location = "https:dustin210.github.io/Dustin210.github.io-Word-Guess-Game/"
+    });
+
+    $("#crystal").on("click", function(){
+        window.location = "https://dustin210.github.io/Dustin210.github.io-unit-4-game/"
+       });
+
+       $("#trivia").on("click", function(){
+        window.location = "https://dustin210.github.io/Dustin210.github.io-TriviaGame/"
+       });
+
+       $("#project").on("click", function(){
+        window.location = "https://dustin210.github.io/Project-1/"
+       });
+
+       $("#github").on("click", function(){
+        window.location = "https://github.com/Dustin210"
+       });
+
+       $("#linkedin").on("click", function(){
+        window.location = "https://www.linkedin.com/in/dustin-martinez-53b408135/"
+       });
+
+       
